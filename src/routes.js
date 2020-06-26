11586754f8f66ac07c070ae0ded8446d6df25cc1
Caiocp <ccarvalho451@gmail.com>
2020-06-26 17:13:49 -0300

    add app routes

---
+++ src/routes.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import Initial from "./pages/Initial";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Dashboard from "./pages/Dashboard";
+import NewTask from "./pages/NewTask";
+
+// import { Container } from './styles';
+
+function Routes() {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" component={Initial} exact />
+        <Route path="/login" component={Login} />
+        <Route path="/register" component={Register} />
+        <Route path="/dashboard" component={Dashboard} />
+        <Route path="/task" component={NewTask} />
+      </Switch>
+    </Router>
+  );
+}
+
+export default Routes;
